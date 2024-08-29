05710227d1aa39ab3c8f4f48c8e5cbb5efc4ca89
MD. RAKIBUL HASAN <aporbo.hasan@gmail.com>
2024-08-29 16:16:18 -0400

    delete old files

--- src/components/Separator.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import styles from './Separator.module.css';
-
-const Separator = ({ src, alt, className }) => {
-  return <img className={`${styles.separator} ${className}`} src={src} alt={alt} />;
-};
-export default Separator;
