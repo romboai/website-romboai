3c88d904648d0af2492a4b9e5cd6458a0a0f8359
Zaza <zanda.andre@gmail.com>
2025-12-23 01:11:19 +0100

    Performance (#98)

--- tests/e2e/blog.spec.js
+++ tests/e2e/blog.spec.js
@@ -4,11 +4,11 @@ test("blog: list loads and first post page renders", async ({ page }) => {
   await page.goto("/blog/");
   await expect(page.locator("body")).toContainText(/Welcome to RomboAI Blog/i);
 
-  // Click the first blog post link (exclude the blog index itself)
-  const firstPostLink = page
-    .locator('a[href^="/blog/"]')
-    .filter({ hasNot: page.locator('a[href="/blog/"]') })
-    .first();
+  // Click the first blog post link (avoid navbar/footer links like /blog/).
+  // Blog list comes from `_includes/blog_articles.html` (cards within the main content).
+  const firstPostLink = page.locator(
+    'main section.container-fluid.bg-white .card a[href^="/blog/"]:not([href="/blog/"]):not([href="/blog"])'
+  ).first();
 
   await expect(firstPostLink).toHaveAttribute("href", /\/blog\/.+\//);
   await firstPostLink.click();
